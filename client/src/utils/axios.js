7545ccb83f0441771345a6646175db9a17108108
Amlan <amlannandy5@gmail.com>
2021-04-03 16:13:01 +0530

    removed console log

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -11,7 +11,6 @@ axiosInstance.interceptors.request.use(config => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  console.log(token + ', ' + config.url);
   return config;
 });
 
