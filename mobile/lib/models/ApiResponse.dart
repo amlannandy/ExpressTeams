4771db31000a618d33c06b9611f36963f80dec7c
Amlan <amlannandy5@gmail.com>
2021-06-30 09:51:41 +0530

    setup dio and login route

--- mobile/lib/models/ApiResponse.dart
+++ mobile/lib/models/ApiResponse.dart
@@ -3,7 +3,14 @@ class ApiResponse {
   String? message;
   List<String>? errors;
 
-  ApiResponse(this.success, this.message, this.errors);
+  ApiResponse(bool success, {String? message, List<String>? errors}) {
+    this.success = success;
+    if (this.success == true) {
+      this.message = message;
+    } else {
+      this.errors = errors;
+    }
+  }
 
   ApiResponse.fromJSON(Map<String, dynamic> json) {
     this.success = json['success'] ?? false;
--- mobile/lib/services/ApiProvider.dart
+++ mobile/lib/services/ApiProvider.dart
@@ -1,12 +1,41 @@
 import 'package:dio/dio.dart';
+
+import 'package:express_teams/config.dart';
 import 'package:express_teams/models/ApiResponse.dart';
+import 'package:express_teams/services/TokenProvider.dart';
 
 class ApiProvider {
-  Dio? dio;
+  late Dio _dio;
 
   ApiProvider() {
-    this.dio = Dio();
+    _dio = Dio();
+    _dio.options.baseUrl = API_URL;
+    _dio.interceptors.clear();
+    _dio.interceptors.add(AccessTokenInterceptor());
   }
 
-  Future<ApiResponse> login(String email, String password) async {}
+  Future<ApiResponse> login(String email, String password) async {
+    try {
+      final postData = {'email': email, 'password': password};
+      final res = await _dio.post('/auth/login', data: postData);
+      final data = res.data;
+      return ApiResponse.fromJSON(data);
+    } catch (e) {
+      print(e);
+      return ApiResponse(false, errors: ['Error during authentication']);
+    }
+  }
+}
+
+class AccessTokenInterceptor extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    String? token = await TokenProvider.getAccessToken();
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+      handler.next(options);
+    }
+    super.onRequest(options, handler);
+  }
 }
